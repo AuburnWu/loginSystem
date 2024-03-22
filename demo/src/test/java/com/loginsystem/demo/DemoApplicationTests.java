e409fb874a0d44a65ba790a04448fca78258135a
auburn <hunterwu0530@yahoo.com>
2024-03-22 16:10:12 +0800

    Initial commit

---
+++ demo/src/test/java/com/loginsystem/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
